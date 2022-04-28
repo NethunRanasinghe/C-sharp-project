f0340be423a1a247d7b946b8d3f87e0a14c678ab
xyzrw <sajeewadw@gmail.com>
2022-04-28 14:52:50 +0530

    2.52

--- C-sharp-project/Login - New User - FPW/Login.Designer.cs
+++ C-sharp-project/Login - New User - FPW/Login.Designer.cs
@@ -49,6 +49,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.login_bpanel.SuspendLayout();
             this.login_tpanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -293,6 +294,16 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // login_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -301,6 +312,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(880, 415);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -346,5 +358,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
--- C-sharp-project/Login - New User - FPW/Login.cs
+++ C-sharp-project/Login - New User - FPW/Login.cs
@@ -195,5 +195,11 @@ namespace C_sharp_project
             ProfitNloss profitNloss = new ProfitNloss();
             profitNloss.Show();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            form_sales form_Sales = new form_sales();
+            form_Sales.Show();
+        }
     }
 }
